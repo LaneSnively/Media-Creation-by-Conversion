831a72cc968e65427de6fa8c58e6018eac1ec31f
Lane Snively <lsnively@uwyo.edu>
2023-03-07 19:33:47 -0700

    rename variables

--- MCBC/app/src/main/java/com/example/mediacreationbyconversion/FirstFragment.java
+++ MCBC/app/src/main/java/com/example/mediacreationbyconversion/FirstFragment.java
@@ -42,7 +42,7 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.totextinput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)

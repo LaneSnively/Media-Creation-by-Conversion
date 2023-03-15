cc465b76e02b2675c775c811632de2e3f9f2ced8
Lane Snively <lsnively@uwyo.edu>
2023-03-15 02:23:26 -0600

    remove unused import

--- MCBC/app/src/main/java/com/example/mediacreationbyconversion/DrawingView.java
+++ MCBC/app/src/main/java/com/example/mediacreationbyconversion/DrawingView.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Environment;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;

dbc841bf6c78f8da7503b23e10360a09ee392abf
Lane Snively <lsnively@uwyo.edu>
2023-03-15 10:15:34 -0600

    reset text before navigating to firstFragment

--- MCBC/app/src/main/java/com/example/mediacreationbyconversion/SecondFragment.java
+++ MCBC/app/src/main/java/com/example/mediacreationbyconversion/SecondFragment.java
@@ -38,14 +38,18 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.tokeyboardinput.setOnClickListener(view1 -> NavHostFragment.findNavController(SecondFragment.this)
-                .navigate(R.id.action_SecondFragment_to_FirstFragment));
+        binding.tokeyboardinput.setOnClickListener(view1 -> {
+            text = "";
+            sendDataToOtherFragment(text);
+            NavHostFragment.findNavController(SecondFragment.this)
+                    .navigate(R.id.action_SecondFragment_to_FirstFragment);
+        });
 
         binding.submit.setOnClickListener(v -> {
             text = binding.inputtext.getText().toString();
+            sendDataToOtherFragment(text);
             NavHostFragment.findNavController(SecondFragment.this)
                     .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            sendDataToOtherFragment(text);
         });
     }
 

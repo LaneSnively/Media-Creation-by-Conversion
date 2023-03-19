a4b36211ca2b581febff76673f6c5453980da4f2
Lane Snively <lsnively@uwyo.edu>
2023-03-19 07:17:03 -0600

    add buttons

--- MCBC/app/src/main/java/com/example/mediacreationbyconversion/MainActivity.java
+++ MCBC/app/src/main/java/com/example/mediacreationbyconversion/MainActivity.java
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding.keycolorlayout.setOnClickListener(v -> {
+
+        });
         View view = binding.getRoot();
         setContentView(view);
     }

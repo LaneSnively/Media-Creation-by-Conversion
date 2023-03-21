a21464e58105efa1c073d182a9e551f149915e38
Lane Snively <lsnively@uwyo.edu>
2023-03-21 12:05:25 -0600

    begin text file selection

--- MCBC/app/src/main/java/com/example/mediacreationbyconversion/SecondFragment.java
+++ MCBC/app/src/main/java/com/example/mediacreationbyconversion/SecondFragment.java
@@ -1,8 +1,11 @@
 package com.example.mediacreationbyconversion;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,10 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.mediacreationbyconversion.databinding.FragmentFirstBinding;
 import com.example.mediacreationbyconversion.databinding.FragmentSecondBinding;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 import java.util.Stack;
 
@@ -58,6 +65,47 @@ public class SecondFragment extends Fragment {
             NavHostFragment.findNavController(SecondFragment.this)
                     .navigate(R.id.action_SecondFragment_to_FirstFragment);
         });
+
+        binding.selectfile.setOnClickListener(v -> {
+            readFile();
+        });
+    }
+
+    private static final int READ_REQUEST_CODE = 42;
+
+    public void readFile() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain"); // Only show text files in the file picker
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = resultData.getData();
+            String path = uri.getPath();
+            if (path != null && path.endsWith(".txt")) {
+                String text = readTextFromUri(uri);
+                // Do something with the text
+            }
+        }
+    }
+
+    private String readTextFromUri(Uri uri) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            InputStream inputStream = requireActivity().getContentResolver().openInputStream(uri);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
     }
 
     @Override

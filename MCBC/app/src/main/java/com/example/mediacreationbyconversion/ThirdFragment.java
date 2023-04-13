c5b1b7e017bd341fb9900f2482da810fb7c0b5bd
Lane Snively <lsnively@uwyo.edu>
2023-04-13 12:40:04 -0600

    add toast

--- MCBC/app/src/main/java/com/example/mediacreationbyconversion/ThirdFragment.java
+++ MCBC/app/src/main/java/com/example/mediacreationbyconversion/ThirdFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -129,6 +130,10 @@ public class ThirdFragment extends Fragment {
                     restoreDrawing();
                     binding.drawingView.invalidate();
                 }
+                if(text.length() > 5000)
+                    Toast.makeText(getContext(),
+                            "loading large text brushes can take a while",
+                            Toast.LENGTH_LONG).show();
             }
         }, 10);
     }
